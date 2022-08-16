4011c084ea6a2d4991a4740a5c674aa5c1e4dadb
JohnWL <34081873+John-WL@users.noreply.github.com>
2022-08-16 18:39:51 -0400

    barebone image follow

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,50 @@
 <template>
     <div class="app-content app-background-color">
-        <img class="app-background-logo" src="@/../assets/logo/icon-v5.png" />
+        <img class="app-background-logo" src="@/../assets/logo/icon-v5.png"
+            :style="{transform:`translate(${this.x - 125}px, ${this.y - 125}px)`}" />
     </div>
-    <div class="app-content background-transparent-color">
-        <NestedWidgetContainer :widgetStackingType="this.WidgetStackingType.HORIZONTAL" ratio="80%">
-            <NestedWidgetContainer :widgetStackingType="this.WidgetStackingType.VERTICAL" ratio="85%">
-                <BaseWidget>
-                    <PlayfieldArea 
-                        :hitObjects="this.hitObjects" 
-                        :circleSize="4" 
-                        :shortcutListener="this.shortcutListener" 
-                        :mouseListener="this.mouseListener" 
-                        :actionHistory="this.actionHistory" 
-                        backgroundImageSrc="C:/Users/Plads/Documents/GitHub/Ozu-Beatmap-Toolset/editor/ozu-editor/assets/bg_test/sakura.jpeg" 
-                        imageBrightness="10%" 
-                    />
-                </BaseWidget>
-                <BaseWidget>
-                    <TimelineBar 
-                        :timelineCursorPosition="1000"
-                    />
-                </BaseWidget>
-            </NestedWidgetContainer>
-            <NestedWidgetContainer :widgetStackingType="this.WidgetStackingType.VERTICAL" ratio="30%">
-                <BaseWidget>
-                </BaseWidget>
-                <BaseWidget>
-                </BaseWidget>
-            </NestedWidgetContainer>
-        </NestedWidgetContainer>
+    <div class="app-content background-transparent-color"
+        @mousemove="this.mouseMove">
+        
     </div>
 </template>
 
 <script>
-    import PlayfieldArea from '@/../src/app/ui/widget/playfield/PlayfieldArea.vue';
-    import ShortcutListener from '@/../src/util/user_input/ShortcutListener.js';
-    import CursorPosition from '@/../src/util/user_input/CursorPosition.js';
-    import ActionHistory from '@/../src/util/action/ActionHistory.js';
-    import BaseWidget from '@/../src/app/ui/widget/generic/BaseWidget.vue';
-    import NestedWidgetContainer from '@/../src/app/ui/widget/generic/NestedWidgetContainer.vue';
+    //import PlayfieldArea from '@/../src/app/ui/widget/playfield/PlayfieldArea.vue';
+    //import ShortcutListener from '@/../src/util/user_input/ShortcutListener.js';
+    //import CursorPosition from '@/../src/util/user_input/CursorPosition.js';
+    //import ActionHistory from '@/../src/util/action/ActionHistory.js';
+    //import BaseWidget from '@/../src/app/ui/widget/generic/BaseWidget.vue';
+    //import NestedWidgetContainer from '@/../src/app/ui/widget/generic/NestedWidgetContainer.vue';
     import { WidgetStackingType } from '@/../src/app/ui/widget/generic/NestedWidgetStackingType.js';
-    import Vector2 from '@/../src/util/math/vector/Vector2.js';
-    import TimelineBar from '@/../src/app/ui/widget/timeline/TimelineBar.vue';
+    //import Vector2 from '@/../src/util/math/vector/Vector2.js';
+    //import TimelineBar from '@/../src/app/ui/widget/timeline/TimelineBar.vue';
 
     export default {
         name: 'App',
         components: {
-            PlayfieldArea,
-            BaseWidget,
-            NestedWidgetContainer,
-            TimelineBar
+            //PlayfieldArea,
+            //BaseWidget,
+            //NestedWidgetContainer,
+            //TimelineBar
         },
         data() {
             return {
                 hitObjects: [],
-                shortcutListener: new ShortcutListener(),
-                mouseListener: new CursorPosition(new Vector2(0, 0)),
-                actionHistory: new ActionHistory(),
+                //shortcutListener: new ShortcutListener(),
+                //mouseListener: new CursorPosition(new Vector2(0, 0)),
+                //actionHistory: new ActionHistory(),
                 WidgetStackingType,
                 ratio: '50%',
                 x: 0,
+                y: 0,
             };
+        },
+        methods: {
+            mouseMove(event) {
+                this.x = event.clientX;
+                this.y = event.clientY;
+            }
         },
         beforeUnmount() {
             this.shortcutListener.unregister();
@@ -84,8 +68,6 @@
         position: absolute;
         width: 250px;
         height: 250px;
-        left: 50%;
-        top: 50%;
         filter: grayscale(1) opacity(0.25) contrast(0.9);
         transform: translate(-50%, -50%);
         -webkit-user-drag: none;
